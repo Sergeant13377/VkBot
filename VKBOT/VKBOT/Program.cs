6cf8468fa17e8310645e3c11f57bae931a5fc776
AirCrack <Aircrack2016@gmail.com>
2016-08-30 00:33:08 +0300

    Login, Password autorithation

--- VKBOT/VKBOT/Program.cs
+++ VKBOT/VKBOT/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VkNet;
+using VkNet.Enums.Filters;
 
 namespace VKBOT
 {
     class Program
     {
+        public static VkApi api = new VkApi();
+        private readonly static string login = "380932144377";
         static void Main(string[] args)
         {
             Console.WriteLine("TEST");
+            var password = Security.ReadPassword();
+            api.Authorize(new ApiAuthParams() { Login = login, Password = password, ApplicationId = 5578797, Settings = Settings.All });
+            Console.WriteLine("Started.");
         }
     }
 }
