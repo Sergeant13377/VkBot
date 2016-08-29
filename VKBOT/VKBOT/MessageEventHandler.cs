f41c3be61375006d9e9077d4ecba9f3d5dbf3dd1
AirCrack <Aircrack2016@gmail.com>
2016-08-30 01:01:04 +0300

    Auth and write message to me.

---
+++ VKBOT/VKBOT/MessageEventHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace VKBOT
+{
+    class MessageEventHandler
+    {
+        static public uint totalMessages = 0;
+        VkApi _api = new VkApi();
+        IReadOnlyCollection<Message> _oldMessages = null;
+
+        public MessageEventHandler(VkApi api)
+        {
+            _api = api;
+        }
+
+        public Tuple<Message, bool> ReadMessage()
+        {
+            recheck:
+            Thread.Sleep(2000);
+            MessagesGetObject Messages = _api.Messages.GetDialogs(new MessagesDialogsGetParams() { Count = 20, Unread = true, });
+            totalMessages = Messages.TotalCount;
+            //totalUnreadMessages = Messages.Unread.Value;
+            IReadOnlyCollection<Message> newMessages = Messages.Messages;
+            if (_oldMessages == null) _oldMessages = newMessages;
+            if (_oldMessages != newMessages)
+            {
+                foreach (var Message in newMessages)
+                {
+                    if (!_oldMessages.Contains(Message))
+                    {
+                        if (Message.Body.ToLower().StartsWith("!bot"))
+                        {
+                            _oldMessages = newMessages;
+                            bool IsChat = false;
+                            try { if (!string.IsNullOrEmpty(Message.ChatId.Value.ToString())) IsChat = true; } catch (Exception) { }
+                            return Tuple.Create<Message, bool>(Message, IsChat);
+                        }
+                    }
+                }
+            }
+            goto recheck;
+        }
+    }
+}
--- VKBOT/VKBOT/Program.cs
+++ VKBOT/VKBOT/Program.cs
@@ -1,19 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using VkNet;
 using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
 
 namespace VKBOT
 {
     class Program
     {
         public static VkApi api = new VkApi();
-        private readonly static string login = "380932144377";
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST");
+            Console.Write("Login: ");
+            string login = Console.ReadLine();
+            Console.Write("Password: ");
             var password = Security.ReadPassword();
             api.Authorize(new ApiAuthParams() { Login = login, Password = password, ApplicationId = 5578797, Settings = Settings.All });
             Console.WriteLine("Started.");
+            api.Messages.Send(new MessagesSendParams() { UserId = 372575398, Message = "Bot#" + new Random().Next(0, 256) + "\n" + "I'm use your bot." }); //random pass
+            MessageEventHandler eh = new MessageEventHandler(api);
+            Console.WriteLine("Message write.");
+            Thread.Sleep(3000);
+            Environment.Exit(0);
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Tuple<Message, bool> receivedMessage = eh.ReadMessage();
+            //        if (!receivedMessage.Item2)
+            //        {
+            //            GetReply(receivedMessage.Item1.UserId.Value, false, receivedMessage.Item1);
+            //        }
+            //        else
+            //        {
+            //            GetReply(receivedMessage.Item1.ChatId.Value, true, receivedMessage.Item1);
+            //        }
+            //    }
+            //    catch (Exception err)
+            //    {
+            //        Console.WriteLine("Main: " + err.Message);
+            //        Console.WriteLine("Trying reboot :/");
+            //        Thread.Sleep(800);
+            //        api.Authorize(new ApiAuthParams() { Login = login, Password = password, ApplicationId = 5578797, Settings = Settings.All });
+            //        Console.WriteLine("Rebooted?");
+            //    }
+            //}
         }
+        //private static void GetReply(long id, bool isChat, Message message)
+        //{
+        //    string[] request = message.Body.Split(' ');
+        //    string reply = "";
+        //    List<string> _class = request[0].Split('.').ToList<string>();
+        //    Console.WriteLine(id + ": " + message.Body);
+        //    if (isChat)
+        //    {
+        //        api.Messages.Send(new MessagesSendParams() { ChatId = id, Message = "Bot#" + new Random().Next(0, 256) + ":\n " + reply });
+        //    }
+        //    else
+        //    {
+        //        api.Messages.Send(new MessagesSendParams() { UserId = id, Message = "Bot#" + new Random().Next(0, 256) + ":\n " + reply });
+        //    }
+        //}
     }
+
 }
